812024c88a9099eab213c830bbf9736c6e8011ad
USER <jsdev04@gmail.com>
2023-06-07 03:34:57 +0600

    Displayed a list of users on the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { User } from "./types";
+import UserForm from "./components/UserForm";
+import UserItem from "./components/UserItem";
+import './App.css'
 
-function App() {
+const App: React.FC = () => {
+  const [users, serUsers] = useState<User[]>([]);
+
+  const addUser = (user: User) => {
+    serUsers([...users, user]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row mt-3">
+        <UserForm addUser={addUser} />
+        <div className="col">
+          <h1>Users</h1>
+          {users.map((user, index) => (
+            <UserItem key={index} user={user} />
+          ))}
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
